6a25d52d173c928df531138cfe739bab5be75952
A-Mack27 <mackattack0408@gmail.com>
2025-09-05 17:59:59 -0700

    Inital commit to my resume website
    - The initial template of my site, currently non-functional

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="max-w-4xl mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">
+          Welcome to My Resume
+        </h1>
+        <p className="text-lg text-gray-600">
+          This is the home page of my resume website.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
