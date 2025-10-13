a26b6c733cf323b669e726e679e683c6916a9a56
A-Mack27 <mackattack0408@gmail.com>
2025-10-13 00:55:26 -0700

    Completed education page - Added extra stack items to demonstrate a few other things I've worked with

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -104,7 +104,110 @@ export default function Education() {
         </table>
         </div>
         
-        
+        <br />
+        <h2>Tools & Technologies</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4">
+          <div>
+            <h3>IDEs and Code Editors</h3>
+            <ul className="content-list">
+              <li>VS Code</li>
+              <li>Visual Studio</li>
+              <li>Eclipse</li>
+              <li>Cursor</li>
+            </ul>
+            
+            <h3>Frameworks</h3>
+            <ul className="content-list">
+              <li>Spring Boot</li>
+              <li>Express.js</li>
+              <li>.NET Core</li>
+              <li>ASP.NET</li>
+              <li>React</li>
+              <li>Angular</li>
+            </ul>
+            
+            <h3>Build and Project Management Tools</h3>
+            <ul className="content-list">
+              <li>Maven</li>
+              <li>Gradle</li>
+              <li>npm</li>
+              <li>Vite</li>
+            </ul>
+            
+            <h3>Frontend Styling and UI Tools</h3>
+            <ul className="content-list">
+              <li>Tailwind CSS</li>
+              <li>Bootstrap</li>
+            </ul>
+            
+            <h3>API Tools and Documentation</h3>
+            <ul className="content-list">
+              <li>Swagger</li>
+              <li>Postman</li>
+            </ul>
+            
+            <h3>Testing Frameworks</h3>
+            <ul className="content-list">
+              <li>JUnit</li>
+              <li>Jest</li>
+            </ul>
+            
+            <h3>Networking, Security, and Analysis Tools</h3>
+            <ul className="content-list">
+              <li>Wireshark</li>
+              <li>Nmap</li>
+              <li>Cisco Packet Tracer</li>
+              <li>OpenVAS</li>
+            </ul>
+          </div>
+          
+          <div>
+            <h3>Operating Systems</h3>
+            <ul className="content-list">
+              <li>Windows</li>
+              <li>Linux (Ubuntu, Kali)</li>
+            </ul>
+            
+            <h3>Virtualization and Containerization</h3>
+            <ul className="content-list">
+              <li>VMware</li>
+              <li>VirtualBox</li>
+              <li>Hyper-V</li>
+              <li>Docker</li>
+            </ul>
+            
+            <h3>Databases</h3>
+            <ul className="content-list">
+              <li>MySQL</li>
+              <li>PostgreSQL</li>
+              <li>MongoDB</li>
+              <li>MAMP</li>
+            </ul>
+            
+            <h3>Web Servers</h3>
+            <ul className="content-list">
+              <li>Nginx</li>
+              <li>Apache</li>
+            </ul>
+            
+            <h3>Cloud Platforms</h3>
+            <ul className="content-list">
+              <li>Microsoft Azure</li>
+              <li>Google Cloud (GCP)</li>
+              <li>AWS</li>
+            </ul>
+            
+            <h3>Version Control and Collaboration</h3>
+            <ul className="content-list">
+              <li>Git</li>
+              <li>GitHub</li>
+              <li>GitLab</li>
+              <li>JIRA</li>
+              <li>Confluence</li>
+              <li>Atlassian</li>
+            </ul>
+          </div>
+        </div>
     </>
   );
 }
