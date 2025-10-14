94e6ce0dae6310e9912bc7d0b58a463e14409013
A-Mack27 <mackattack0408@gmail.com>
2025-10-13 18:35:20 -0700

    Completed demos page - Added a coming soon message to the demos page - Added a link to my minesweeper repository in place of a demo

--- src/pages/Demos.tsx
+++ src/pages/Demos.tsx
@@ -2,9 +2,20 @@ export default function Demos() {
   return (
     <>
       <h1>Demos</h1>
-      <h2>Test Quicksand Font</h2>
-      <h3>Test Quicksand Font</h3>
-      <p>This is the demos page of my resume website.</p>
+      <hr />
+      <br />
+      <h2>Coming Soon!</h2>
+      <p>More interactive demos will be added here soon. In the meantime, check out the repository for 
+        my C# Minesweeper implementation!</p>
+      <a 
+        href="https://github.com/A-Mack27/Minesweeper" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="text-[#FE9920] hover:text-[#DCDCDD] transition-colors duration-200 underline"
+      >
+        Minesweeper Repository
+      </a>
+      <br />
     </>
   );
 }
