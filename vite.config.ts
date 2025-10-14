109f7c822c6cfe350cc8f8261a37e3b6af97e62f
A-Mack27 <mackattack0408@gmail.com>
2025-10-13 21:54:44 -0700

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Andrew-Mack-Resume/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
